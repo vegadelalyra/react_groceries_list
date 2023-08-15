e870db473529a2522a020eca2e54b9b9a7b4a5f7
Vegadelalyra <lyradeperseo@gmail.com>
2023-08-15 01:07:52 -0500

    feat: tutorial 07

--- src/Content.js
+++ src/Content.js
@@ -1,30 +1,64 @@
 import { useState } from "react"
+import { FaTrashAlt } from 'react-icons/fa'
+
+const itemsList = [
+    {
+        id: 1,
+        checked: true,
+        item: "One half pound bag of Cocoa Covered Almonds Unsalted"    
+    }, 
+    {
+        id: 2,
+        checked: false,
+        item: "Item 2"  
+    }, 
+    {
+        id: 3,
+        checked: false,
+        item: "Item 3"  
+    }
+]
 
 const Content = () => {
-    const [name, setName] = useState('Daniel')
-    const [count, setCount] = useState(0)
-    
-    const handleNameChange = () => {
-        const names = ['Rlyeh', 'Veroz', 'Daniel', 'Pollito']
-        const int = Math.floor(Math.random() * names.length)
-        setName(names[int])
-    } 
-    
-    const handleClick = () => {
-        setCount(count + 1)
-        console.log(count)
+    const [items, setItems] = useState(itemsList)
+
+    const handleCheck = id => {
+        const listItems = items.map(item => item.id === id 
+            ? { ...item, checked: !item.checked }
+            : item
+        ); setItems(listItems)
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems))
     }
 
-    const handleClick2 = () => {
-        console.log(count)
+    const handleDelete = id => {
+        const listItems = items.filter(item => item.id !== id)
+        setItems(listItems)
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems))
     }
 
-    return (
+    return !items.length 
+    ? ( <main> <p style={{ marginTop: '2rem' }}>Your list is empty.</p> </main>) 
+    : (
         <main>
-            <p onDoubleClick={handleClick}>Hello {name}!</p>
-            <button onClick={handleNameChange}>Change name!</button>
-            <button onClick={handleClick}>Click it!</button>
-            <button onClick={handleClick2}>Click it!</button>
+            {/* maps needs to have a key attribute with a value in react */}
+            <ul>{items.map(item => (
+                <li className="item" key={item.id}>
+                    <input 
+                        type="checkbox" 
+                        onChange={() => handleCheck(item.id)}
+                        checked={item.checked} 
+                    />
+                    <label
+                        style={item.checked ? { textDecoration: 'line-through'} : null }
+                        onDoubleClick={() => handleCheck(item.id)}
+                    >{item.item}</label>
+                    <FaTrashAlt 
+                        role="button" 
+                        tabIndex="0" 
+                        onClick={() => handleDelete(item.id)}
+                    />
+                </li>
+            ))}</ul>
         </main>
     )
 }
