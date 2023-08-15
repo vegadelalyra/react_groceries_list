031b8633ca9dbddc36a63a4403fc67caafcff2af
Vegadelalyra <lyradeperseo@gmail.com>
2023-08-14 23:02:42 -0500

    feat: tutorial 05

--- src/Content.js
+++ src/Content.js
@@ -5,9 +5,24 @@ const Content = () => {
         return names[int]
     }
     
+    const handleClick = () => {
+        console.log('You clicked it')
+    }
+
+    const handleClick2 = name => {
+        console.log(name + ' was clicked')
+    }
+
+    const handleClick3 = event => {
+        console.log(event.target.innerText)
+    }
+
     return (
         <main>
-            <p>Hello {handleNameChange()}!</p>
+            <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+            <button onClick={handleClick}>Click it!</button>
+            <button onClick={() => handleClick2('Daniel')}>Click it!</button>
+            <button onClick={e => handleClick3(e)}>Click it!</button>
         </main>
     )
 }
